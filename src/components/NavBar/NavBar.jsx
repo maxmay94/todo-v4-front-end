2a63ba3477bf506f1125262601eb4a8bbf36f4ba
maxmay94 <maxmay94@gmail.com>
2022-06-05 17:00:45 -0400

    navbar done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,25 +2,24 @@ import { Link } from 'react-router-dom'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <>
+    <div className='bg-gray-700 hover:bg-gray-600'>
       {user ?
         <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
+          <ul className='flex'>
+            <li className='flex-1 ml-5' >Welcome, {user.name}</li>
+            <li className='flex-1 ml-14' ><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            <li className='mr-5' ><Link to="/changePassword">Change Password</Link></li>
           </ul>
         </nav>
       :
         <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+          <ul className='flex'>
+            <li className='flex-1 ml-5'><Link to="/login">Log In</Link></li>
+            <li className='mr-5'><Link to="/signup">Sign Up</Link></li>
           </ul>
         </nav>
       }
-    </>
+    </div>
   )
 }
 
---
+++ src/services/todoService.js
@@ -0,0 +1,42 @@
+import * as tokenService from './tokenService'
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/todos`
+
+export async function getAllTodos() {
+  try {
+
+  } catch(err) {
+    throw err
+  }
+}
+
+export async function createTodo() {
+  try {
+
+  } catch(err) {
+    throw err
+  }
+}
+
+export async function deleteTodos(todoId) {
+  try {
+
+  } catch(err) {
+    throw err
+  }
+}
+
+export async function updateTodo(todoId, title) {
+  try {
+
+  } catch(err) {
+    throw err
+  }
+}
+
+export async function doTodo(todoId) {
+  try {
+
+  } catch(err) {
+    throw err
+  }
+}
